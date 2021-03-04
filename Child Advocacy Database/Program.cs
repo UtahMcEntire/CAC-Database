147cf51d6b0f4b1ebc821c39b21bce6e1d14a637
cag7a <cag7a@mtmail.mtsu.edu>
2021-03-04 10:40:38 -0600

    Added Database. Query in progress

--- Child Advocacy Database/Program.cs
+++ Child Advocacy Database/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Child_Advocacy_Database
 {
@@ -17,6 +18,18 @@ namespace Child_Advocacy_Database
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            SqlConnection connection = new SqlConnection();
+            // Adds the connection string for the Database.
+            // The @ escapes the \ in the file locations
+            connection.ConnectionString =
+                @"Data Source=(LocalDB).\MSSQLLocalDB;" + 
+                @"AttachDbFilename=|DataDirectory|.\Database1.mdf;" +
+                "Integrated Security=True;";
+            connection.Open();
+            string queryString = "";
+            SqlCommand command = new SqlCommand(queryString, connection);
+
         }
     }
 }
---
+++ Child Advocacy Database/Queries/queryChildLastName.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE dbo.getChildLastName @childLstName varchar(75)
+AS
+BEGIN
+	SELECT * FROM dbo.[Case Database] WHERE childLastName = @childLstName
+END
+GO
