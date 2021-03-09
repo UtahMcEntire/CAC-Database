74f593f5c89b772338dbf5563cdf849bcf56ed5b
Utah <46671092+UtahMcEntire@users.noreply.github.com>
2021-03-09 10:27:52 -0600

    Buttons Linked
    Linked buttons to proper forms.
    Quit button checks if other windows are open.
    Exit button still closes all windows regardless.

--- Child Advocacy Database/dashboard.Designer.cs
+++ Child Advocacy Database/dashboard.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Child_Advocacy_Database
 {
-    partial class dashboard
+    partial class Dashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -30,90 +30,93 @@ namespace Child_Advocacy_Database
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
             this.searchTip = new System.Windows.Forms.ToolTip(this.components);
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.PleaseTextBox = new System.Windows.Forms.TextBox();
+            this.WelcomeTextBox = new System.Windows.Forms.TextBox();
             this.addTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // AddButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(95, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 81);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add New Case File(s)";
-            this.addTip.SetToolTip(this.button1, "Add a new case file to the database");
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.AddButton.Location = new System.Drawing.Point(95, 154);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(184, 81);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add New Case File(s)";
+            this.addTip.SetToolTip(this.AddButton, "Add a new case file to the database");
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
-            // button3
+            // SearchButton
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button3.Location = new System.Drawing.Point(295, 154);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(183, 81);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Search Database";
-            this.searchTip.SetToolTip(this.button3, "Search for a case file to view, edit, or delete");
-            this.button3.UseVisualStyleBackColor = true;
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.SearchButton.Location = new System.Drawing.Point(295, 154);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(183, 81);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search Database";
+            this.searchTip.SetToolTip(this.SearchButton, "Search for a case file to view, edit, or delete");
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.QueryButton_Click);
             // 
             // searchTip
             // 
             this.searchTip.Tag = "";
             this.searchTip.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
-            // button4
+            // QuitButton
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button4.Location = new System.Drawing.Point(440, 313);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(123, 34);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Quit";
-            this.button4.UseVisualStyleBackColor = true;
+            this.QuitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.QuitButton.Location = new System.Drawing.Point(440, 313);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(123, 34);
+            this.QuitButton.TabIndex = 3;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
             // 
-            // textBox1
+            // PleaseTextBox
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox1.Location = new System.Drawing.Point(95, 121);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(383, 16);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "Please choose an action below:";
+            this.PleaseTextBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.PleaseTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PleaseTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.PleaseTextBox.Location = new System.Drawing.Point(95, 121);
+            this.PleaseTextBox.Name = "PleaseTextBox";
+            this.PleaseTextBox.Size = new System.Drawing.Size(383, 16);
+            this.PleaseTextBox.TabIndex = 4;
+            this.PleaseTextBox.Text = "Please choose an action below:";
             // 
-            // textBox2
+            // WelcomeTextBox
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox2.Location = new System.Drawing.Point(95, 72);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(383, 21);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "Welcome to the CAC File System Manager!";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.WelcomeTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.WelcomeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.WelcomeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.WelcomeTextBox.Location = new System.Drawing.Point(95, 72);
+            this.WelcomeTextBox.Multiline = true;
+            this.WelcomeTextBox.Name = "WelcomeTextBox";
+            this.WelcomeTextBox.Size = new System.Drawing.Size(383, 21);
+            this.WelcomeTextBox.TabIndex = 5;
+            this.WelcomeTextBox.Text = "Welcome to the CAC File System Manager!";
+            this.WelcomeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // dashboard
+            // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 359);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WelcomeTextBox);
+            this.Controls.Add(this.PleaseTextBox);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.AddButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "dashboard";
+            this.MaximizeBox = false;
+            this.Name = "Dashboard";
             this.Text = "CAC File System Dashboard";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -122,12 +125,12 @@ namespace Child_Advocacy_Database
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button SearchButton;
         private System.Windows.Forms.ToolTip searchTip;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.TextBox PleaseTextBox;
+        private System.Windows.Forms.TextBox WelcomeTextBox;
         private System.Windows.Forms.ToolTip addTip;
     }
 }
--- Child Advocacy Database/dashboard.cs
+++ Child Advocacy Database/dashboard.cs
@@ -10,21 +10,63 @@ using System.Windows.Forms;
 
 namespace Child_Advocacy_Database
 {
-    public partial class dashboard : Form
+    public partial class Dashboard : Form
     {
-        public dashboard()
+        public Dashboard()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void AddButton_Click(object sender, EventArgs e)
         {
-
+            var AddForm = new Form1(); // Bring up add form onclick
+            AddForm.FormClosed += Form1_FormClosing;
+            AddButton.Enabled = false;
+            AddForm.Show();
         }
 
         private void toolTip1_Popup(object sender, PopupEventArgs e)
         {
 
+        }
+
+        private void QueryButton_Click(object sender, EventArgs e)
+        {
+            var QueryForm = new Query(); // Bring up search form onclick
+            QueryForm.FormClosed += Form1_FormClosing;
+            SearchButton.Enabled = false;
+            QueryForm.Show();
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            AddButton.Enabled = true; // re-enables add button on dashboard
+            this.Show();
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            bool Form1_Open = false;
+            FormCollection fc = Application.OpenForms;
+            foreach (Form frm in fc)
+            {
+                //iterate through forms to see if "form1" form or "Query" form is open
+                if (frm.Name == "Form1")
+                {
+                    Form1_Open = true;
+                    MessageBox.Show("The case file editor is open. Please save any unsaved data and close that window before exiting the program to avoid possible data loss.", "Case File Editor Open", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                }
+                if (frm.Name == "Query")
+                {
+                    Form1_Open = true;
+                    MessageBox.Show("The search window is open. Please save close that window before exiting the program to avoid possible data loss.", "Case File Search Open", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                }
+
+            }
+            if (!Form1_Open)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
         }
     }
 }
