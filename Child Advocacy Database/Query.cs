4a76d88a68b5e93337a54ed05ea25e35d339a7d0
jamesbellottie <79927528+jamesbellottie@users.noreply.github.com>
2021-03-16 11:40:57 -0500

    removed commented section

--- Child Advocacy Database/Query.cs
+++ Child Advocacy Database/Query.cs
@@ -24,31 +24,3 @@ namespace Child_Advocacy_Database
         }
     }
 }
-﻿/*using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Child_Advocacy_Database
-{
-    public partial class Query : Form
-    {
-        public Query()
-        {
-            InitializeComponent();
-        }
-
-        private void addRemoveBtn_Click(object sender, EventArgs e)
-        {
-            var AddForm = new AddCaseForm(); // Bring up add form onclick
-            AddForm.Show();
-            Close();
-        }
-    }
-}
-*/
